270ab5d7ecd2449916c750d3e9ff13dbd6285068
Jennifer Nist <jennifernist@aol.com>
2014-01-24 00:17:29 +0100

    changed format

--- Mega_TFT_DataLogger_RTClock/Mega_TFT_DataLogger_RTClock.ino
+++ Mega_TFT_DataLogger_RTClock/Mega_TFT_DataLogger_RTClock.ino
@@ -99,16 +99,16 @@ void loop()
 
 
 ///////////////////////////////////////////////////////////////
-            /// Functions ////
+/// Functions ////
 ///////////////////////////////////////////////////////////////
 
 void initRTC() {
-    //setSyncProvider() causes the Time library to synchronize with the
-    //external RTC by calling RTC.get() every five minutes by default.
-    setSyncProvider(RTC.get);
-    Serial.print("RTC Sync");
-    if (timeStatus() != timeSet) Serial.print(" FAIL!");
-    Serial.println();
+  //setSyncProvider() causes the Time library to synchronize with the
+  //external RTC by calling RTC.get() every five minutes by default.
+  setSyncProvider(RTC.get);
+  Serial.print("RTC Sync");
+  if (timeStatus() != timeSet) Serial.print(" FAIL!");
+  Serial.println();
 }
 
 ///////////////////////////////////////////////////////////////
@@ -183,36 +183,36 @@ void writeStaticText() {
 ///////////////////////////////////////////////////////////////////
 
 void updateTimeFromSerial() {
-      //check for input to set the RTC, minimum length is 12, i.e. yy,m,d,h,m,s
-    if (Serial.available() >= 12) {
-        //note that the tmElements_t Year member is an offset from 1970,
-        //but the RTC wants the last two digits of the calendar year.
-        //use the convenience macros from Time.h to do the conversions.
-        int y = Serial.parseInt();
-        if (y >= 100 && y < 1000)
-            Serial.println("Error: Year must be two digits or four digits!");
-        else {
-            if (y >= 1000)
-                tm.Year = CalendarYrToTm(y);
-            else    //(y < 100)
-                tm.Year = y2kYearToTm(y);
-            tm.Month = Serial.parseInt();
-            tm.Day = Serial.parseInt();
-            tm.Hour = Serial.parseInt();
-            tm.Minute = Serial.parseInt();
-            tm.Second = Serial.parseInt();
-            t = makeTime(tm);
-            RTC.write(tm);
-            setTime(t);        
-            Serial.print("RTC set to: ");
-            Serial.println(dateString);
-            //dump any extraneous input
-            while (Serial.available() > 0) Serial.read();
-        }
+  //check for input to set the RTC, minimum length is 12, i.e. yy,m,d,h,m,s
+  if (Serial.available() >= 12) {
+    //note that the tmElements_t Year member is an offset from 1970,
+    //but the RTC wants the last two digits of the calendar year.
+    //use the convenience macros from Time.h to do the conversions.
+    int y = Serial.parseInt();
+    if (y >= 100 && y < 1000)
+      Serial.println("Error: Year must be two digits or four digits!");
+    else {
+      if (y >= 1000)
+        tm.Year = CalendarYrToTm(y);
+      else    //(y < 100)
+      tm.Year = y2kYearToTm(y);
+      tm.Month = Serial.parseInt();
+      tm.Day = Serial.parseInt();
+      tm.Hour = Serial.parseInt();
+      tm.Minute = Serial.parseInt();
+      tm.Second = Serial.parseInt();
+      t = makeTime(tm);
+      RTC.write(tm);
+      setTime(t);        
+      Serial.print("RTC set to: ");
+      Serial.println(dateString);
+      //dump any extraneous input
+      while (Serial.available() > 0) Serial.read();
     }
+  }
 } 
 
-////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 void getTemp() {
 
@@ -230,26 +230,26 @@ void getTemp() {
 //////////////////////////////////////////////////////////////////
 
 void tftPrintTime() {
-      t = now();
-    if (t != tLast) {
-        tLast = t;
-        
-        dateString = String(day(t));
-        dateString += ".";
-        dateString += month(t); 
-        dateString += ".";
-        dateString += year(t);
-        dateString += " - ";
-        dateString += hour(t);
-        dateString += ":";
-        dateString += minute(t);
-        dateString += ":";
-        dateString += second(t);
+  t = now();
+  if (t != tLast) {
+    tLast = t;
 
-        tft.fillRect(0, 32, 126, 7, ST7735_BLACK);
-        tft.setCursor(0,32);
-        tft.println(dateString);
-    }
+    dateString = String(day(t));
+    dateString += ".";
+    dateString += month(t); 
+    dateString += ".";
+    dateString += year(t);
+    dateString += " - ";
+    dateString += hour(t);
+    dateString += ":";
+    dateString += minute(t);
+    dateString += ":";
+    dateString += second(t);
+
+    tft.fillRect(0, 32, 126, 7, ST7735_BLACK);
+    tft.setCursor(0,32);
+    tft.println(dateString);
+  }
 }  
 
 //////////////////////////////////////////////////////////////////
@@ -285,7 +285,7 @@ void tftPrintTemp() {
 /////////////////////////////////////////////////////////
 
 void locateTempSensors() {
-  
+
   tft.setCursor(0,0);
   tft.fillScreen(ST7735_BLACK);
   // locate devices on the 1-wire bus
@@ -413,4 +413,5 @@ char * floatToString(char * outstr, double val, byte precision, byte widthp){
 
   return outstr;
 }
+
 
